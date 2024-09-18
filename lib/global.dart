41f0a81ae16b3bdc4de66c0425bcec1aea1317c5
Abhay kumar bhumihar <abhayrai637@gmail.com>
2024-09-18 17:26:24 +0530

    flutter hive added

--- lib/global.dart
+++ lib/global.dart
@@ -1,6 +1,7 @@
 import 'package:bloc_learning/service_locator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'common/local_storage/hive/hive_helper.dart';
 import 'common/services/storage_service.dart';
 
 class Global {
@@ -9,6 +10,7 @@ class Global {
   static Future init() async {
     setupLocator();
     WidgetsFlutterBinding.ensureInitialized();
+    await HiveHelper().init();
      // Initialize the service locator
     // Initialize Firebase
     await Firebase.initializeApp(
@@ -27,3 +29,27 @@ class Global {
     storageService = await StorageService().init();
   }
 }
+/**
+ * add data await HiveHelper().addData('productsBox', Product(id: 1, name: 'Laptop'));
+
+    get All Data
+
+    List<Product> products = await HiveHelper().getAllData<Product>('productsBox');
+
+    Updating data
+
+    await HiveHelper().updateData('productsBox', 1, Product(id: 1, name: 'Updated Laptop'));
+
+
+    Deleting data:
+
+    await HiveHelper().deleteDataByKey('productsBox', 1);
+
+    Check if a key exists:
+    bool exists = await HiveHelper().containsKey('productsBox', 1);
+
+
+
+
+ * */
+
